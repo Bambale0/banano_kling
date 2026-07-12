662e8d1862663009d77b4e29d0a58ab54e3a53f6
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-07-12 07:01:15 +0300

    Register repeat compatibility and notification workers

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -9,21 +9,27 @@ from .batch_generation import router as batch_generation_router
 from .common import router as legacy_common_router
 from .generation import router as generation_router
 from .image_analyzer import router as image_analyzer_router
+from .notification_campaigns import router as notification_campaigns_router
 from .payments import router as payments_router
+from .repeat_result_compat import router as repeat_result_compat_router
 from .support import router as support_router
 
 # main.py already places common_router last. Keep that contract while ensuring
-# support FSM handlers run before broad legacy common handlers.
+# specific support/repeat handlers run before broad legacy common handlers.
 common_router = Router()
+common_router.include_router(notification_campaigns_router)
+common_router.include_router(repeat_result_compat_router)
 common_router.include_router(support_router)
 common_router.include_router(legacy_common_router)
 
 __all__ = [
-    "common_router",
-    "generation_router",
-    "payments_router",
     "admin_router",
     "batch_generation_router",
+    "common_router",
+    "generation_router",
     "image_analyzer_router",
+    "notification_campaigns_router",
+    "payments_router",
+    "repeat_result_compat_router",
     "support_router",
 ]
